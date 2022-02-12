0f461c6ce8d3d435ffbd96613617db1d1ac57b65
rmaicle <rmaicle@gmail.com>
2022-02-12 21:50:05 +0800

    Do not declare variable as read-only

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ declare SCRIPTNAME=${0##*/}
 declare SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 declare CURRENT_DIR=$(pwd)
 
-declare -r PROGRAM="pandoc"
+declare PROGRAM="pandoc"
 
 
 
